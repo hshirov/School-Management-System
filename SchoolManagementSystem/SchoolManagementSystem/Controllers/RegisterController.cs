7098e6b1f4dccedcca0689ba937b0ab31374c92d
Hristo Shirov <hshirov@abv.bg>
2020-04-30 20:20:37 +0300

    Made register in on controller

---
+++ SchoolManagementSystem/SchoolManagementSystem/Controllers/RegisterController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SchoolManagementSystem.Controllers
+{
+    public class RegisterController : Controller
+    {
+        // GET: Register
+        public ActionResult Index()
+        {
+            return RedirectToAction("Student");
+        }
+
+        public ActionResult Student()
+        {
+            return View();
+        }
+
+        public ActionResult Teacher()
+        {
+            return View();
+        }
+    }
+}
