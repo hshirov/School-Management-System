5171b452a16c0dd75f1536ee00cdcee13d812ad3
Hristo Shirov <hshirov@abv.bg>
2020-04-30 17:06:49 +0300

    added bootstrap

--- SchoolManagementSystem/SchoolManagementSystem/App_Start/BundleConfig.cs
+++ SchoolManagementSystem/SchoolManagementSystem/App_Start/BundleConfig.cs
@@ -23,7 +23,7 @@ namespace SchoolManagementSystem
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-slate.css",
                       "~/Content/site.css"));
         }
     }
