8c7ad7ad5773837a446f0a2f55c9bf86b6fa0595
Веселин Инзов <veselin.inzov@gmail.com>
2020-04-30 16:30:56 +0300

    Update Teacher.cs model

--- SchoolManagementSystem/Data/Models/Teacher.cs
+++ SchoolManagementSystem/Data/Models/Teacher.cs
@@ -8,25 +8,26 @@ namespace Data.Models
     public class Teacher
     {
         public int Id { get; set; }
+
         [DisplayName("First Name")]
-        [Required(ErrorMessage = "This field is required.")]
+        [Required(ErrorMessage = "First Name is Required.")]
         public string FirstName { get; set; }
 
         [DisplayName("Last Name")]
-        [Required(ErrorMessage = "This field is required.")]
+        [Required(ErrorMessage = "Last Name is Required.")]
         public string LastName { get; set; }
 
         [DisplayName("Email")]
-        [Required(ErrorMessage = "This field is required.")]
+        [Required(ErrorMessage = "Email is Required.")]
         public string Email { get; set; }
 
         [DisplayName("Password")]
-        [Required(ErrorMessage = "This field is required.")]
+        [Required(ErrorMessage = "Password is Required.")]
         [DataType(DataType.Password)]
         public string PasswordHash { get; set; }
 
         [DisplayName("Mobile")]
-        [Required(ErrorMessage = "This field is required.")]
+        [Required(ErrorMessage = "Mobile is Required.")]
         public string Mobile { get; set; }
 
         [DisplayName("Subject")]
