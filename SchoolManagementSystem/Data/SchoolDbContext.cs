1ff58ab35ccd09651d431a17ac98156fdddc7915
Hristo Shirov <hshirov@abv.bg>
2020-04-30 16:52:39 +0300

    changed dbcontext

--- SchoolManagementSystem/Data/SchoolDbContext.cs
+++ SchoolManagementSystem/Data/SchoolDbContext.cs
@@ -11,6 +11,11 @@ namespace Data
     public class SchoolDbContext : DbContext
     {
         public SchoolDbContext() : base("SchoolDb") { }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            Database.SetInitializer<SchoolDbContext>(null);
+            base.OnModelCreating(modelBuilder);
+        }
         public DbSet<Student> Students { get; set; }
         public DbSet<Teacher> Teachers { get; set; }
     }
