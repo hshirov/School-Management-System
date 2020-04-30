6bc22dccc1e15e8e39df0d4fc5dcc4b7d1ef7c9e
Hristo Shirov <hshirov@abv.bg>
2020-04-30 15:46:46 +0300

    added vadidate fields

--- SchoolManagementSystem/Data/Models/Student.cs
+++ SchoolManagementSystem/Data/Models/Student.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace Data.Models
 {
@@ -6,24 +7,31 @@ namespace Data.Models
     {
         public int Id { get; set; }
         [DisplayName("First Name")]
+        [Required(ErrorMessage = "This field is required.")]
         public string FirstName { get; set; }
 
         [DisplayName("Last Name")]
+        [Required(ErrorMessage = "This field is required.")]
         public string LastName { get; set; }
 
         [DisplayName("Email")]
+        [Required(ErrorMessage = "This field is required.")]
         public string Email { get; set; }
 
         [DisplayName("Password")]
+        [Required(ErrorMessage = "This field is required.")]
         public string PasswordHash { get; set; }
 
         [DisplayName("Mobile")]
+        [Required(ErrorMessage = "This field is required.")]
         public string Mobile { get; set; }
 
         [DisplayName("Class Number")]
+        [Required(ErrorMessage = "This field is required.")]
         public int ClassNumber { get; set; }
 
         [DisplayName("Class Letter")]
+        [Required(ErrorMessage = "This field is required.")]
         public string ClassLetter { get; set; }
     }
 }
--- SchoolManagementSystem/Data/Models/Teacher.cs
+++ SchoolManagementSystem/Data/Models/Teacher.cs
@@ -1,6 +1,7 @@
 ﻿
 
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace Data.Models
 {
@@ -8,21 +9,27 @@ namespace Data.Models
     {
         public int Id { get; set; }
         [DisplayName("First Name")]
+        [Required(ErrorMessage = "This field is required.")]
         public string FirstName { get; set; }
 
         [DisplayName("Last Name")]
+        [Required(ErrorMessage = "This field is required.")]
         public string LastName { get; set; }
 
         [DisplayName("Email")]
+        [Required(ErrorMessage = "This field is required.")]
         public string Email { get; set; }
 
         [DisplayName("Password")]
+        [Required(ErrorMessage = "This field is required.")]
         public string PasswordHash { get; set; }
 
         [DisplayName("Mobile")]
+        [Required(ErrorMessage = "This field is required.")]
         public string Mobile { get; set; }
 
         [DisplayName("Subject")]
+        [Required(ErrorMessage = "This field is required.")]
         public string Subject { get; set; }
     }
 }
