1cb0534397219a4ac7c6444f5118b78ebca89373
Hristo Shirov <hshirov@abv.bg>
2020-05-04 21:55:05 +0300

    classroom

---
+++ SchoolManagementSystem/SchoolManagementSystem/Controllers/ClassroomController.cs
@@ -0,0 +1,29 @@
+﻿using Business;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SchoolManagementSystem.Controllers
+{
+    public class ClassroomController : Controller
+    {
+        private readonly StudentBll _studentBll = new StudentBll();
+        // GET: Classroom
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Classmates()
+        {
+            return View(_studentBll.GetStudentsFromClass((int)Session["studentID"]));
+        }
+
+        public ActionResult Classes()
+        {
+            return View(_studentBll.GetStudents());
+        }
+    }
+}
