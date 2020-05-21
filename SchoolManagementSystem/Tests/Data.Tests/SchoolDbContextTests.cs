0ab058de55c1400ca6dd9b3dfcf92061545c595a
Веселин Инзов <veselin.inzov@gmail.com>
2020-05-21 21:28:37 +0300

    Add Business and Data tests

---
+++ SchoolManagementSystem/Tests/Data.Tests/SchoolDbContextTests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+
+namespace Tests.Data.Tests
+{
+    [TestFixture]
+    public class SchoolDbContextTests
+    {
+        [Test]
+        public void TestMethod()
+        {
+            // TODO: Add your test code here
+            var answer = 42;
+            Assert.That(answer, Is.EqualTo(42), "Some useful error message");
+        }
+    }
+}
