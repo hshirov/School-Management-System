cef0568c6f8eb38bc25c226276cc510406af520a
Веселин Инзов <veselin.inzov@gmail.com>
2020-05-26 12:44:35 +0300

    Adding Tests project.

---
+++ SchoolManagementSystem/Tests/Controllers/AdminPanelControllerTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests.Controllers
+{
+    [TestClass]
+    public class AdminPanelControllerTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ SchoolManagementSystem/Tests/Controllers/LoginControllerTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests.Controllers
+{
+    [TestClass]
+    public class LoginControllerTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
